d25cee51a7c52d8089867353e6234054dd1a3aab
Samiya <skmcintosh20@gmail.com>
2022-04-11 19:56:57 -0700

    connected db

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import Cors from "cors";
+
+import Cards from "./dbCards.js";
 
 //  App Config
 const app = express();
@@ -8,6 +11,8 @@ const connection_url =
 	"mongodb+srv://admin:Wolves22@cluster0.afoom.mongodb.net/datemedb?retryWrites=true&w=majority";
 
 // Middlewares
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -17,6 +22,28 @@ mongoose.connect(connection_url, {
 
 // API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.post("/dateme/cards", (req, res) => {
+	const dbCard = req.body;
+
+	Cards.create(dbCard, (err, data) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(201).send(data);
+		}
+	});
+});
+
+app.get("/dateme/cards", (req, res) => {
+	Cards.find((err, data) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(data);
+		}
+	});
+});
 
 // Listener
 
